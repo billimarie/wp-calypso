dd879e0f4d799c6961217dc04d75cee44fca75d6
retrofox <rdsuarez@gmail.com>
2019-08-13 17:45:35 -0300

    set blocksInPreview not defined as default

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control.js
@@ -33,17 +33,25 @@ const TemplateSelectorItem = ( props ) => {
 		dynamicPreview = false,
 		preview,
 		previewAlt = '',
-		blocksInPreview = 10,
+		blocksInPreview,
 	} = props;
 
 	const itemRef = useRef( null );
 	const [ dynamicCssClasses, setDynamicCssClasses ] = useState( 'is-rendering' );
-	const [ blocksAmount, setBlocksAmount ] = useState( blocksInPreview );
 
-	const blocks = useMemo( () => ( dynamicPreview ?
-		parseBlocks( rawBlocks ).slice( 0, blocksAmount ) :
-		[]
-	) , [ rawBlocks, blocksAmount, dynamicPreview ] );
+	const blocks = useMemo( () => {
+		if ( ! dynamicPreview ) {
+			return [];
+		}
+
+		const parsedBlocks = parseBlocks( rawBlocks );
+		if ( blocksInPreview ) {
+			return parsedBlocks.slice( 0, blocksInPreview );
+		}
+
+		return parsedBlocks;
+
+	}, [ rawBlocks, blocksInPreview, dynamicPreview ] );
 
 	useLayoutEffect( () => {
 		const timerId = setTimeout( () => {
@@ -55,6 +63,7 @@ const TemplateSelectorItem = ( props ) => {
 			}
 
 			// Try to pick up the editor styles wrapper element.
+			// And move the `.editor-styles-wrapper` class out of the preview.
 			const editorStylesWrapperEl = el.querySelector( '.editor-styles-wrapper' );
 			if ( editorStylesWrapperEl ) {
 				setTimeout( () => {
@@ -62,9 +71,6 @@ const TemplateSelectorItem = ( props ) => {
 				}, 0 );
 				setDynamicCssClasses( 'editor-styles-wrapper' );
 			}
-
-			// experimental.
-			setBlocksAmount( 100 );
 		}, 0 );
 
 		// Cleanup
@@ -93,8 +99,8 @@ const TemplateSelectorItem = ( props ) => {
 			id={ `${ id }-${ value }` }
 			className="template-selector-control__label"
 			value={ value }
-			onClick={ () => onSelect( value, label, parseBlocks( rawBlocks ) ) }
-			onMouseEnter={ () => onFocus( value, label, dynamicPreview ? blocks : parseBlocks( rawBlocks ) ) }
+			onClick={ () => onSelect( value, label, blocks ) }
+			onMouseEnter={ () => onFocus( value, label, blocks ) }
 			aria-describedby={ help ? `${ id }__help` : undefined }
 		>
 			<div className="template-selector-control__preview-wrap">
